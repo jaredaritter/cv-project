d0259cd9fa8a797cf93e11b9e7ec99601a8f6a03
jared <38332939+jaredaritter@users.noreply.github.com>
2020-10-22 10:07:54 -0400

    Created new edit components

---
+++ src/components/EducationEdit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class EducationEdit extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>Education Edit</div>;
+  }
+}
+
+export default EducationEdit;
---
+++ src/components/PracticalEdit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PracticalEdit extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>Practical Edit</div>;
+  }
+}
+
+export default PracticalEdit;
