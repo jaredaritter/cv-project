a80d8a77eaecbd75f27cf0296419360ba7741839
jared <38332939+jaredaritter@users.noreply.github.com>
2020-11-13 12:11:53 -0500

    Moved default state values to the checlLocalStorage function to plug in if localstorage not found

--- src/utils/Helpers.js
+++ src/utils/Helpers.js
@@ -3,9 +3,34 @@ function addToLocalStorage(name, data) {
 }
 
 function checkStorageAndUpdate(name, setter) {
+  const defaultValue = {
+    general: {
+      name: 'Jared',
+      email: 'awesome@email.com',
+      phone: '555-555-5555',
+    },
+    education: [
+      {
+        name: 'University of Awesome',
+        study: 'Awesomeness',
+        startDate: '2000',
+        endDate: 'current',
+      },
+    ],
+    practical: {
+      name: 'Awesome Inc.',
+      position: 'Engineer of Awesomeness',
+      responsibilities: 'Making things awesome.',
+      startDate: '2005',
+      endDate: 'current',
+    },
+  };
+
   if (localStorage.getItem(name)) {
     const storedData = JSON.parse(localStorage.getItem(name));
     setter(storedData);
+  } else {
+    setter(defaultValue[name]);
   }
 }
 
